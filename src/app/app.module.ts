c64849435e038273a4b138cdb6eb15367f681e28
Jeycob <jchavezc25@upao.edu.pe>
2022-11-05 18:06:05 -0500

    subir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InscriptionsComponent } from './inscriptions/inscriptions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionsComponent,
   ],
   imports: [
     BrowserModule
